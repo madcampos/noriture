14719ae6ea4ea39948f812308b29a45e87d8ec01
madcampos <madcampos@outlook.com>
2022-04-08 01:09:06 -0400

    feat: add base app files

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { registerSW } from 'virtual:pwa-register';
+
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+const ONE_HOUR_IN_MS = 60 * 60 * 1000;
+
+const updateSW = registerSW({
+	onNeedRefresh: () => {
+		// TODO: show refresh popup
+		// TODO: call updateSW();
+	},
+	onOfflineReady: () => {
+		// TODO: show offline ready message
+	},
+	onRegistered: (workerRegistration) => {
+		if (workerRegistration?.active) {
+			setInterval(() => {
+				void workerRegistration.update();
+			}, ONE_HOUR_IN_MS);
+		}
+	}
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
