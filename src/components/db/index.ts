1ec57e3b440a21a90d227f27b46387912f2e096a
madcampos <madcampos@outlook.com>
2022-04-12 02:15:29 -0400

    feat: add feed items table to db

--- src/components/db/index.ts
+++ src/components/db/index.ts
@@ -1,15 +1,18 @@
 import Dexie, { type Table } from 'dexie';
 import type { Feed } from '../feeds';
+import type { FeedItem } from '../feeds/feedItem';
 
 const DATABASE_VERSION = import.meta.env.APP_VERSION;
 
 export class Database extends Dexie {
-  feeds!: Table<Feed>;
+  feeds!: Table<Feed, Feed['id']>;
+  feedItems!: Table<FeedItem, FeedItem['id']>;
 
   constructor() {
     super('noriture');
     this.version(DATABASE_VERSION).stores({
-      feeds: '&id, &name, &url, category'
+      feeds: '&id, name, &url, category, type, displayType',
+      feedItems: '&id, title, &url, date, feedId'
     });
   }
 }
