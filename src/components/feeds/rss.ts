0415df9a2c2bd42136dee776e05042167495b95e
madcampos <madcampos@outlook.com>
2022-04-12 02:16:44 -0400

    feat: implement base rss functionality

--- src/components/feeds/rss.ts
+++ src/components/feeds/rss.ts
@@ -1,1 +1,24 @@
-export type Placeholder = unknown;
+import type { Ref } from 'vue';
+import type { Feed } from './feed';
+import type { FeedItem } from './feedItem';
+
+interface RssFeed extends Feed {
+	items: FeedItem[],
+	type: Ref<'rss'>
+}
+
+export async function readRssFeed(url: string) {
+	// TODO: check correctly for proxy
+	const response = await fetch(`https://thingproxy.freeboard.io/fetch/${url}`, {
+		method: 'GET',
+		credentials: 'omit',
+		redirect: 'follow'
+	});
+
+	const text = await response.text();
+	const xml = new window.DOMParser().parseFromString(text, 'text/xml');
+
+	console.log(xml);
+
+	// TODO: finish implementation and parsing
+}
