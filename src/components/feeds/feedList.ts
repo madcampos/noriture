13ca3ce7ccb5499803e4eb198b2a8e3095d36300
madcampos <madcampos@outlook.com>
2022-04-12 02:16:24 -0400

    fix: upfate feed list

--- src/components/feeds/feedList.ts
+++ src/components/feeds/feedList.ts
@@ -1,7 +1,7 @@
 import { shallowRef, watch } from 'vue';
 import type { Feed } from './feed';
 
-const feedList = shallowRef<Record<string, Feed | undefined>>({});
+const feedList = shallowRef<Record<Feed['id'], Feed>>({});
 
 export function useFeedList() {
 	if (Object.keys(feedList.value).length === 0) {
@@ -9,6 +9,7 @@ export function useFeedList() {
 	}
 
 	watch(feedList, (feeds) => {
+		console.log(feeds);
 		// TODO: save feeds IDB
 	});
 
