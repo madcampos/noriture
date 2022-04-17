6eb4b3e465a1a693b08b3b88f0504bbac8664287
madcampos <madcampos@outlook.com>
2022-04-17 06:30:44 -0400

    feat: implement basic db operations

--- globals.d.ts
+++ globals.d.ts
@@ -3,37 +3,24 @@
 type Serializable = string | number | boolean | Serializable[] | { [key: string]: Serializable };
 
 interface PackageJsonVariables {
-		homepage: string,
-		displayName: string,
-		shortName: string,
-		description: string,
-		keywords: string[],
-		author: {
-			name: string,
-			email: string
-		},
-		version: string,
+	homepage: string,
+	displayName: string,
+	shortName: string,
+	description: string,
+	keywords: string[],
+	author: {
+		name: string,
+		email: string
+	},
+	version: string,
 	[key: string]: Serializable
 }
 
 interface ImportMetaEnv {
-	readonly PUBLIC_URL: PackageJsonVariables.homepage,
-
-	readonly APP_NAME: PackageJsonVariables.displayName,
-	readonly APP_SHORT_NAME: PackageJsonVariables.shortName,
-	readonly APP_DESCRIPTION: PackageJsonVariables.description,
-	readonly APP_KEYWORDS: string,
-	readonly APP_AUTHOR: PackageJsonVariables.author.name,
-	readonly APP_VERSION: PackageJsonVariables.version,
-
-	readonly THEME_COLOR: string,
-	readonly BACKGROUND_COLOR: string,
-
-	readonly APPLE_ICON: string,
-	readonly SMALL_ICON: string,
-	readonly SMALL_ICON_BG: string,
-	readonly LARGE_ICON: string,
-	readonly LARGE_ICON_BG: string
+	readonly BASE_URL: string,
+	readonly MODE: 'development' | 'production',
+	readonly PROD: boolean,
+	readonly DEV: boolean
 }
 
 interface ImportMeta {
--- src/components/db/index.ts
+++ src/components/db/index.ts
@@ -1,20 +1,85 @@
-import Dexie, { type Table } from 'dexie';
+import Dexie, { liveQuery, type Table } from 'dexie';
+import { type Ref, unref } from 'vue';
 import type { Feed } from '../feeds';
 import type { FeedItem } from '../feeds/feedItem';
 
-const DATABASE_VERSION = import.meta.env.APP_VERSION;
+const DATABASE_VERSION = 1;
 
-export class Database extends Dexie {
-  feeds!: Table<Feed, Feed['id']>;
-  feedItems!: Table<FeedItem, FeedItem['id']>;
+type SavedFeed = Omit<Feed, 'items'>;
 
-  constructor() {
-    super('noriture');
-    this.version(DATABASE_VERSION).stores({
-      feeds: '&id, name, &url, category, type, displayType',
-      feedItems: '&id, title, &url, date, feedId'
-    });
-  }
+class Database extends Dexie {
+	feeds!: Table<SavedFeed, SavedFeed['id']>;
+	feedItems!: Table<FeedItem, FeedItem['id']>;
+
+	constructor() {
+		super('noriture');
+		this.version(DATABASE_VERSION).stores({
+			feeds: '&id, name, &url, category, type, displayType',
+			feedItems: '&id, title, &url, date, feedId'
+		});
+	}
 }
 
-export const database = new Database();
+const database = new Database();
+
+export function listUnreadItems(feedId: string) {
+	return liveQuery(async () => database.feedItems.where('feedId').equals(feedId).and((item) => !item.read).toArray());
+}
+
+export async function saveFeedItems(items: FeedItem[] | Ref<FeedItem[]>) {
+	const itemsValue = unref(items);
+
+	return database.feedItems.bulkPut(itemsValue.map((item) => ({
+		feedId: item.feedId,
+		id: item.id,
+		title: item.title,
+		url: item.url,
+		date: item.date,
+		author: item.author,
+		content: item.content,
+		image: item.image,
+		read: item.read,
+		tags: [...item.tags],
+		...(item.media && { media: item.media.map((media) => ({
+			url: media.url,
+			size: media.size,
+			mimeType: media.mimeType,
+			type: media.type
+		})) })
+	})));
+}
+
+export async function getFeedItem(id: string) {
+	return database.feedItems.get({ id }) as Promise<FeedItem>;
+}
+
+export function listFeeds() {
+	return liveQuery(async () => database.feeds.toArray());
+}
+
+export async function getFeed(id: string) {
+	return database.feeds.get({ id });
+}
+
+export async function saveFeed(feed: Feed | Ref<Feed>) {
+	const feedValue = unref(feed);
+
+	await saveFeedItems(feedValue.items);
+
+	return database.feeds.add({
+		feedUrl: feedValue.siteUrl,
+		id: feedValue.id,
+		name: feedValue.name,
+		categories: [...feedValue.categories],
+		type: feedValue.type,
+		displayType: feedValue.displayType,
+		siteUrl: feedValue.siteUrl,
+		description: feedValue.description,
+		icon: feedValue.icon,
+		lastUpdated: feedValue.lastUpdated,
+		backgroundColor: feedValue.backgroundColor ?? '',
+		color: feedValue.color ?? '',
+		unreadCount: feedValue.unreadCount,
+		unreadItemIds: [...feedValue.unreadItemIds]
+	});
+}
