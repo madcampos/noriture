24b37e6c73952a3a6a0f4f5fc27ad1ed42fed02c
madcampos <madcampos@outlook.com>
2022-04-22 09:54:34 -0400

    chore: reorder imports

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,12 @@
 // Styles
+import '@fontsource/roboto';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
-import { aliases, mdi } from 'vuetify/lib/iconsets/mdi';
-import { en } from 'vuetify/locale';
 
 // Vuetify
 import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi';
+import { en } from 'vuetify/locale';
 
 export default createVuetify({
 	theme: {
