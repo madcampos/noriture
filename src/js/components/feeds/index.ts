807e86af8d38843f65cd51c3d227757d7bbd6faa
madcampos <madcampos@outlook.com>
2022-08-10 02:45:11 -0400

    feat: fix reference to new proxy variables

--- src/js/components/feeds/index.ts
+++ src/js/components/feeds/index.ts
@@ -1,2 +1,2 @@
-export { DEFAULT_ITEM_UPDATE_COUNT, DEFAULT_ITEM_UPDATE_COUNT_LIST, DEFAULT_REFRESH_RATE, DEFAULT_REFRESH_RATE_LIST, useFeedUpdateItemsCount, useRefreshRate } from './refreshRate';
+export { DEFAULT_ITEM_UPDATE_COUNT, DEFAULT_ITEM_UPDATE_COUNT_LIST, DEFAULT_REFRESH_RATE, DEFAULT_REFRESH_RATE_LIST, feedItemUpdateCount, feedRefreshRate } from './refreshRate';
 export type { Feed } from './feed';
