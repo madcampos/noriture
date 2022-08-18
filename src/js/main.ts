78e12164c8c180f8a71345fc40e4d86a1cbeb171
madcampos <madcampos@outlook.com>
2022-08-18 03:24:51 -0400

    feat: add translations

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,2 +1,8 @@
 import './plugins/serviceWorker';
+import { I18n } from './packages/I18n/i18n';
 import './router/index';
+import { Router } from './router/router';
+
+(async () => {
+	await I18n.init();
+})();
