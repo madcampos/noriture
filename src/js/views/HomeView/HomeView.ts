4c53f44d6f0c6ea8525a316c3e76380a868185d9
madcampos <madcampos@outlook.com>
2022-08-18 03:25:11 -0400

    feat: add code to load view

--- src/js/views/HomeView/HomeView.ts
+++ src/js/views/HomeView/HomeView.ts
@@ -1,3 +1,5 @@
+import type { FeedCard } from '../../components/FeedCard/FeedCard';
+import { Database } from '../../db';
 import type { View } from '../../router/router';
 import templateString from './HomeView.html?raw';
 
@@ -27,7 +29,27 @@ export class HomeView implements View {
 		return this.#root;
 	}
 
+	async #loadCards() {
+		const feeds = await Database.listFeeds();
+
+		for (const feed of feeds) {
+			const template = this.#feedCardTemplate.content.cloneNode(true) as HTMLElement;
+			const card = template.querySelector('feed-card') as FeedCard;
+
+			card.feedId = `/feed/${feed.id}`;
+			card.unreadCount = feed.unreadCount;
+
+			(card.querySelector('[slot=title]') as HTMLSpanElement).textContent = feed.name;
+			(card.querySelector('[slot=description]') as HTMLSpanElement).textContent = feed.description ?? '';
+			(card.querySelector('[slot=last-updated]') as HTMLSpanElement).textContent = feed.lastUpdated.toLocaleString();
+			(card.querySelector('[slot=image]') as HTMLImageElement).src = feed.icon ?? '';
+
+			this.#root.appendChild(card);
+		}
+	}
+
 	render() {
-		// TODO: render the feed cards
+		this.#root.querySelectorAll('feed-card').forEach((card) => card.remove());
+		void this.#loadCards();
 	}
 }
