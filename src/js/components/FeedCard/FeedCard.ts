20bec5b65d5eb4938af57e56c287d36a63333ab2
madcampos <madcampos@outlook.com>
2022-08-18 03:22:13 -0400

    feat: update feed card design

--- src/js/components/FeedCard/FeedCard.ts
+++ src/js/components/FeedCard/FeedCard.ts
@@ -1,9 +1,10 @@
 import templateString from './FeedCard.html?raw';
 
 export class FeedCard extends HTMLElement {
-	static get observedAttributes() { return ['feedId']; }
+	static get observedAttributes() { return ['feed-id', 'unread-count']; }
 
 	#root: ShadowRoot;
+	#unreadCount: HTMLSpanElement;
 
 	constructor() {
 		super();
@@ -14,6 +15,34 @@ export class FeedCard extends HTMLElement {
 
 		this.#root = this.attachShadow({ mode: 'closed' });
 		this.#root.appendChild(template.content.cloneNode(true));
+
+		this.#unreadCount = this.#root.querySelector('[slot=unread-count]') as HTMLSpanElement;
+	}
+
+	get feedId() {
+		return this.getAttribute('feed-id') ?? '';
+	}
+
+	set feedId(value: string) {
+		this.setAttribute('feed-id', value);
+	}
+
+	get unreadCount() {
+		return Number.parseInt(this.getAttribute('unread-count') ?? '0');
+	}
+
+	set unreadCount(value: number) {
+		this.setAttribute('unread-count', value.toString());
+	}
+
+	attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+		if (oldValue !== newValue) {
+			if (name === 'feed-id') {
+				this.#root.querySelector('router-link')?.setAttribute('to', newValue);
+			} else if (name === 'unread-count') {
+				this.#unreadCount.textContent = newValue;
+			}
+		}
 	}
 }
 
