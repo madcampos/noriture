9766cb5cf63e87d5e4f616720fe08cbfb0ac440e
madcampos <madcampos@outlook.com>
2024-02-17 04:27:29 -0500

    feat: update proxy server

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,25 @@
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { Hono, type HonoRequest } from 'hono';
 
 const INTERNAL_SERVER_ERROR = 500;
 const BAD_REQUEST = 400;
 const OKAY = 200;
 
 const app = new Hono();
-app.get('/', (context) => context.text('Hello Hono!'));
+
+app.get('/', (context) => context.text('Hello proxy!'));
+
+function getCorsHeaders(request: HonoRequest) {
+	return {
+		'Access-Control-Allow-Origin': request.header('Origin') ?? '*',
+		'Access-Control-Allow-Methods': request.header('Access-Control-Request-Method') ?? 'GET, OPTIONS',
+		'Access-Control-Allow-Headers': request.header('Access-Control-Request-Headers') ?? 'Content-Type',
+		'Access-Control-Expose-Headers': [...request.raw.headers.keys()].join(', ') ?? '',
+		'Access-Control-Max-Age': '86400'
+	};
+}
+
+app.options('/proxy', (context) => new Response('', { status: OKAY, headers: getCorsHeaders(context.req) }));
 
 app.get('/proxy', async (context) => {
 	const url = context.req.query('url');
@@ -39,12 +52,23 @@ app.get('/proxy', async (context) => {
 		const text = await response.text();
 
 		return context.text(text, OKAY, {
-			'Content-Type': contentType,
-			'Access-Control-Allow-Origin': '*',
+			...getCorsHeaders(context.req),
+			'Content-Type': 'text/xml'
 		});
 	} catch (err) {
 		return context.text(err.message, INTERNAL_SERVER_ERROR);
 	}
 });
 
-serve(app);
+
+const HOST = 'localhost';
+const PORT = 3000;
+
+serve({
+	fetch: app.fetch,
+	port: PORT,
+	hostname: HOST
+}).on('listening', () => {
+	// eslint-disable-next-line no-console
+	console.info(`Server is listening on http://${HOST}:${PORT}`);
+});
