836e9b112c4dbd0c046518d571fdfcf33bcfaeb0
madcampos <madcampos@outlook.com>
2024-03-09 20:33:59 -0500

    fix: resolve router link not navigating away

--- src/router/router-link.ts
+++ src/router/router-link.ts
@@ -6,13 +6,18 @@ import { Router } from './router';
 @customElement('router-link')
 export class RouterLink extends LitElement {
 	// eslint-disable-next-line id-length
-	@property({ type: String, reflect: true, attribute: 'router-link' }) to = '/';
+	@property({ type: String, reflect: true, attribute: 'to' }) to = '/';
 
 	#click(evt: MouseEvent) {
 		evt.preventDefault();
 		evt.stopPropagation();
 
-		const target = evt.target as HTMLAnchorElement;
+		const target = evt.currentTarget as HTMLAnchorElement;
+
+		if (!target.matches('a')) {
+			return;
+		}
+
 		const path = target.href;
 
 		if (path) {
