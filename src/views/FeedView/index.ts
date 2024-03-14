6d1174c7f975a68090895e016ff7aef636eca80f
madcampos <madcampos@outlook.com>
2024-03-14 02:28:02 -0400

    feat: implement feed view

--- src/views/FeedView/index.ts
+++ src/views/FeedView/index.ts
@@ -1,38 +1,83 @@
-import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import type { RouterView } from '../../router/router';
+import type { Feed } from '../../packages/Feed/Feed.ts';
+import type { RouteLocation, RouterView } from '../../router/router';
+
+import { html, LitElement, nothing } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import { when } from 'lit/directives/when.js';
+import { Database } from '../../db/index.ts';
 
 @customElement('n-feed-view')
 export class FeedView extends LitElement implements RouterView {
-	navigate() {
+	@state() private feed?: Feed;
+
+	async #loadFeed(feedId: string) {
+		this.feed = await Database.getFeed(feedId);
+	}
+
+	#interceptLinkClick(evt: MouseEvent) {
+		const target = evt.target as HTMLElement;
+
+		if (target.matches('a')) {
+			evt.preventDefault();
+
+			window.open((target as HTMLAnchorElement).href, '_blank');
+		}
+	}
+
+	navigate({ params: { id: feedId } }: RouteLocation<'/feed/:id'>) {
+		void this.#loadFeed(feedId);
+
 		return 'Feed';
 	}
 
 	override render() {
-		// TODO: Add the actual data binding
-		return html`<header>
-			<h1>{{title}}</h1>
-			<aside>
-				<!-- TODO: add unread count -->
-				<span>{{date}}</span>
-				<p>{{description}}</p>
-				<!-- TODO: add feed buttons -->
-			</aside>
-			<picture>
-				<img src="{{image}}" alt="{{title}}">
-			</picture>
-		</header>
-		<template id="item-card">
-			<item-card
-				title="{{title}}"
-				image="{{image}}"
-				link="{{link}}"
-				author="{{author}}"
-				date="{{date}}"
-				tags="{{tags}}"
-			>
-				{{content}}
-			</item-card>
-		</template>`;
+		return html`
+			<n-main-layout feed-id="${this.feed?.id ? this.feed.id : nothing}">
+				<div slot="header">
+					<picture>
+						${when(
+							this.feed?.icon,
+							() => html`<img src="${this.feed?.icon ?? ''}" alt="${this.feed?.name ?? ''}" />`,
+							() => html`<iconify-icon icon="fluent:star-48-regular" inline></iconify-icon>`
+						)}
+					</picture>
+					<h1>${this.feed?.name ?? 'No title'}</h1>
+					<aside>
+						<span>
+							<small>${this.feed?.lastUpdated.toLocaleString() ?? 'Never'}</small>
+							<small>(${this.feed?.unreadCount ?? 0} / ${this.feed?.items.length ?? 0})</small>
+						</span>
+					</aside>
+				</div>
+				<div id="description" @click="${this.#interceptLinkClick}">
+					${when(
+						this.feed?.description,
+						() => unsafeHTML(this.feed?.description ?? ''),
+						() => html`<p>This feed has no description.</p>`
+					)}
+				</div>
+				<ul>
+					${this.feed?.items.map((item) => html`
+						<li>
+							<n-item-card
+								feed-id="${this.feed?.id ?? ''}"
+								item-id="${item.id ?? ''}"
+								title="${item.title ?? 'No title'}"
+								author="${item.author ?? ''}"
+								date="${item.date?.toLocaleString() ?? ''}"
+							>
+								${when(
+									item.image,
+									() => html`<img src="${item.image ?? ''}" alt="${item.title ?? 'No title'}" />`
+								)}
+								${unsafeHTML(item.content)}
+							</n-item-card>
+						</li>
+					`)}
+				</ul>
+			</n-main-layout>
+		`;
 	}
 }
